c2fd29aaea70362a0b6569b48f0201d4de66629b
malena <89429614+malenarivera@users.noreply.github.com>
2022-10-27 18:54:57 -0300

    Update Main.java

--- tp5/punto7/Main.java
+++ tp5/punto7/Main.java
@@ -10,18 +10,38 @@ package universidad.concurrente.tp5.punto7;
  * @author male_
  */
 public class Main {
-    
     public static void main(String[] args) {
-        Thread babuinos []= new Thread[10];
-        Puente p = new Puente(5);
-        for (int i=0;i<babuinos.length;i++){
-            babuinos[i]= new Thread( new Babuino(p));
+        Scanner s= new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de babuinos del lado der");
+        int cantBabuinosD= s.nextInt();
+        System.out.println("Ingrese la cantidad de babuinos del lado izq");
+        int cantBabuinosI =s.nextInt();
+
+
+        Thread babuinos[]= new Thread[cantBabuinosD+cantBabuinosI];
+
+        int limite;
+
+
+        Cuerda c= new Cuerda();
+
+        int j=0;
+
+        for (int i=0;i<cantBabuinosI+cantBabuinosD;i++){
+            if (i<cantBabuinosI){
+                babuinos[i]= new Thread(new Babuino('I',c), "Babuino Izquierda "+i);
+            }
+            else{
+
+                babuinos[i]= new Thread(new Babuino('D',c), "Babuino Derecho "+j);
+                j++;
+            }
         }
-        for (int i=0;i<babuinos.length;i++){
+        for (int i=0;i<cantBabuinosI+cantBabuinosD;i++){
             babuinos[i].start();
         }
+        
+
 
     }
-    
 }
-
